77614010c5daa6a23c50d1ece1337076149a26c7
rigidswarm <rigidswarm@gmail.com>
2021-03-13 15:41:42 +0300

    Container node changes & Cabinet changes

--- Makefile
+++ Makefile
@@ -30,6 +30,9 @@ assets-install:
 assets-dev:
 	docker-compose exec node yarn run dev
 
+assets-prod:
+	docker-compose exec node yarn run prod
+
 assets-watch:
 	docker-compose exec node yarn run watch
 
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    public const CABINET = '/cabinet';
 
     /**
      * The controller namespace for the application.
